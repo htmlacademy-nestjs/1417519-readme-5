10806b7f7fc191b06eefafca5187f21492f6d561
Pronin Maks <maksimpronin1991@icloud.com>
2023-12-14 19:39:44 +0300

    add controller and service

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [BlogUserModule, AuthenticationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
