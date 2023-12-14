9e72afe8b5dc690d2c75071dd9e6505e32d0ec86
Pronin Maks <maksimpronin1991@icloud.com>
2023-12-14 19:27:41 +0300

    add new modules for account

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogUserModule } from './blog-user/blog-user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [BlogUserModule, AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
