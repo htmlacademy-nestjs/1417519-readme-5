d1813014ebbcdeea910b8ed181128fe9b5593eda
Pronin Maks <maksimpronin1991@icloud.com>
2023-12-16 21:25:31 +0300

    check

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDto } from '@project/shared/helpers';
 import { UserRdo } from './rdo/user.rdo';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
@@ -14,5 +16,17 @@ export class AuthenticationController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillDto(UserRdo, newUser.toPOJO());
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillDto(UserRdo, existUser.toPOJO());
   }
 }
