fb8472d60c41964dcfdc70b49d7ef14ba253318c
Pronin Maks <maksimpronin1991@icloud.com>
2024-01-20 16:56:12 +0300

    save

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -7,14 +7,26 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Blog API description')
+    .setVersion('1.0')
+    .build();
+
   const GLOBAL_PREFIX = 'api';
   const PORT = 3000;
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || PORT;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
